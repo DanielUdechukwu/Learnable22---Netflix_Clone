9ea6b42d570f1230a576d6a001bd34e42fd8f9c5
Daniel <danieludechukwu117@gmail.com>
2023-01-13 18:58:05 +0100

    HomePage|Responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let accordion = $('.accordion');
 
-for (let i = 0; i <accordion.length; i++){
+for (let i = 0; i < accordion.length; i++){
   accordion[i].addEventListener("click", function(){
     this.classList.toggle('active');
 
     let panel = this.nextElementSibling;
+    
     if (panel.style.display === 'block'){
       panel.style.display = 'none';
     } 
